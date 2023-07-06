c68ad6dd374af14b28e0373836f212fcb0e61ab0
bugrakocabay <kocabaybugra@gmail.com>
2023-07-06 19:51:05 +0300

    auth service init

---
+++ auth-service/cmd/api/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
